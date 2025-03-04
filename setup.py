e2aa8fcdd9ecc05d1ebbef4a6016f63aa42b0e92
varunwolf <varunmailme07@gmail.com>
2025-03-05 02:27:57 +0530

    First folder structure added my admin

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative AI project',
+    version = '0.0.0',
+    author = 'Varun kumar',
+    author_email = 'varunmailme07@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
